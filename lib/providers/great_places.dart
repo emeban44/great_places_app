c15f465f5e59d578225e5fbc18b8cc99be1f195a
emeban44 <emi.cazorla@hotmail.com>
2021-03-26 17:11:28 +0100

    storing places locally with sqlite working

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:great_places_app/helpers/db_helper.dart';
 
 import '../models/place.dart';
 
@@ -19,6 +21,24 @@ class GreatPlaces with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map((place) => Place(
+              id: place['id'],
+              image: File(place['image']),
+              title: place['title'],
+              location: null,
+            ))
+        .toList();
     notifyListeners();
   }
 }
